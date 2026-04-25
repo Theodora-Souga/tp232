8359a9d9ba45c81c50f90207d2ee96bb245ff93f
Theodora-Souga <moignon237@gmail.com>
2026-04-25 22:05:32 +0100

    Update code.py

--- code.py
+++ code.py
@@ -37,14 +37,14 @@ REGIONS = ["Adamaoua", "Centre", "Est", "Extrême-Nord", "Littoral", "Nord", "No
 FONCTIONS = ["Professeur", "Proviseur", "Directeur de CES", "Vacataire en poste"]
 
 if page == "Formulaire d'Enregistrement":
-    st.header("📝 Collecte des données enseignants")
+    st.header("📝 Collecte des données des enseignants du secondaire au Cameroun")
     st.info("Veuillez remplir tous les champs ci-dessous pour vous enregistrer.")
     
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
-            matricule = st.text_input("Matricule unique", placeholder="Ex: 123456-X").upper()
-            nom = st.text_input("Nom Complet", placeholder="Ex: Jean Dupont")
+            matricule = st.text_input("Matricule unique", placeholder="Ex: 0582349Z").upper()
+            nom = st.text_input("Nom Complet", placeholder="Ex: ATANGANA Bernard")
         with col2:
             fonction = st.selectbox("Fonction occupée", FONCTIONS)
             region = st.selectbox("Région de service", REGIONS)
